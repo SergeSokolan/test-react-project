c0e2937c9999ede18097e9d0b7d6dc8a617a1283
SSokolan <sokolan.sergey@gmail.com>
2018-11-03 16:29:17 +0300

    Updates

---
+++ precache-manifest.cde9655d5039d92bddb8ca273f0eba1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react-project/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "68098f3194cf07343032",
+    "url": "/test-react-project/static/js/runtime~main.68098f31.js"
+  },
+  {
+    "revision": "a602bbb786367fde9bc0",
+    "url": "/test-react-project/static/js/main.a602bbb7.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/test-react-project/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "a602bbb786367fde9bc0",
+    "url": "/test-react-project/static/css/main.7bf34a41.chunk.css"
+  },
+  {
+    "revision": "ec0742bc39e856b207d7adb1ae81d0db",
+    "url": "/test-react-project/index.html"
+  }
+];
