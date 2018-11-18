8d0b9c82e00c5304649eed2a978043efe259a124
SSokolan <sokolan.sergey@gmail.com>
2018-11-18 13:31:40 +0300

    create data-file; data output

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
 import "./App.css";
+import data from "./data/goods.json";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = data;
+  }
+
   render() {
     return (
-      <section class="sec">
-        <div class="cont">
-          <ul class="list">
-            <li class="item">item</li>
-            <li class="item">item</li>
-            <li class="item">item</li>
+      <section className="sec">
+        <div className="cont">
+          <ul className="list">
+            {this.state["cat-food"].map((item, index) => {
+              return (
+                <li className={`item ${item['in-stock'] === false ? 'item_unavailable' : ''}`} key={index}>
+                <span>{item['item-name']}</span>
+                  <h1>{item.title}</h1>
+                  <h2>{item.subtitle}</h2>
+                  <p>{`${item.serving} порций`}</p>
+                  <p>{`${item.gift} мышь в подарок`}</p>
+                  <p>{`${item.weight} кг`}</p>
+                  <p>{item.description}</p>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </section>
